40966a63f8cdcf379d73d66bfcae52659804eb0f
azizosharke <abdelaza@tcd.ie>
2022-04-01 14:33:26 +0100

    edited main for invalid inputs

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         java.io.File file = new java.io.File("stop_times.txt");
-        boolean end = false;
-
+        System.out.println("***********************************************************" +
+                " \n WELCOME TO VANCOUVER BUS SYSTEM DATA   \n" +
+                "***********************************************************\n");
         Scanner input = new Scanner(System.in);
+
         System.out.println("***********************************************************" +
                 " \n PLEASE SELECT ONE OF THE FOLLOWING OPTIONS : (0,1,2,3) \n" +
                 "***********************************************************\n");
@@ -15,6 +17,9 @@ public class Main {
         System.out.println("PRESS [1]TO FIND THE SHORTEST PATHS BETWEEN TWO BUS STOPS");
         System.out.println("PRESS [2] TO SEARCH FOR THE REQUIRED BUS STOP");
         System.out.println("PRESS [3] TO SEARCH FOR THE ARRIVAL TIME FOR THE BUS AT THE STATION");
+        if (!input.hasNextInt()) {                                                         // for invalid inputs
+            System.out.println("THIS IS AN INVALID INPUT. PLEASE TRY AGAIN");
+        }
 
     }
 
