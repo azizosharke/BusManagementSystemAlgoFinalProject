e223b0766e28af2ef89a6a177f0cc1409f85b188
azizosharke <abdelaza@tcd.ie>
2022-04-01 19:10:57 +0100

    Class Search is finished Used new method called Comparable to easily compare objects

--- src/Search.java
+++ src/Search.java
@@ -2,17 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 import static java.lang.Double.MAX_VALUE;
 
-public class Search {
+public class Search implements Comparable<Search> {
     private double shortestPath;
     public String n;
     private Search pathV;
-    private final List<Finder> s;
-
-
+    private final List<Finder> s;                  // used comparable to order objects of the user defined class as it is much
+                                                  // easier to use the interface than making a new method
     public Search(String n){             // randomly generated getters and setters from the IDE
         this.s = new ArrayList<>();
         this.n =n;
         shortestPath = MAX_VALUE;
+    }
+    public int compareTo(Search compare) {
+        return Double.compare(this.shortestPath, compare.shortestPath);
+    }
+    public String toString() {  // to convert the into string and make it readable
+        return n;
     }
     public List<Finder> getPath() {
         return s;
@@ -31,3 +36,5 @@ public class Search {
         this.pathV = d;
     }
 }
+
+// class Search is finished
