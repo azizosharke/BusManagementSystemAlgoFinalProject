0c3027006386e8ff5b3300f8fd6aa5e7a974b15d
azizosharke <abdelaza@tcd.ie>
2022-04-02 15:12:10 +0100

    started doing Q1 using Dijkstra to find shortest path

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,19 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.lang.Double.parseDouble;
+
 public class Graph {
 
+    public Graph() {
+        return;
+    }
+    public Graph(String stopDes, String transfers, String stops) throws FileNotFoundException {
+        File file = new File(stopDes);
+        Scanner scanner = new Scanner(file);
+    }
 }
+
+
