231ed930e7a979a3d210a9627752067ece30b08e
azizosharke <abdelaza@tcd.ie>
2022-04-05 13:09:39 +0100

    connected DirectedEdge to the main and Search

--- src/DirectedEdge.java
+++ src/DirectedEdge.java
@@ -1,30 +1,24 @@
 /**
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *    https://algs4.cs.princeton.edu/44sp/DirectedEdge.java.html
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * https://algs4.cs.princeton.edu/44sp/DirectedEdge.java.html
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
-
-public class DirectedEdge {
-    private final int v;
-    private final int w;
-    private final double weight;
-
-    public DirectedEdge(int v, int w, double weight) {
+public record DirectedEdge(int v, int w, double weight) {
+    public DirectedEdge {
         if (v < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
         if (w < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
         if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
-        this.v = v;
-        this.w = w;
-        this.weight = weight;
-
     }
+
     public int from() {
         return v;
     }
 
     /**
      * Returns the head vertex of the directed edge.
+     *
      * @return the head vertex of the directed edge
      */
     public int to() {
@@ -33,8 +27,10 @@ public class DirectedEdge {
 
     /**
      * Returns the weight of the directed edge.
+     *
      * @return the weight of the directed edge
      */
+    @Override
     public double weight() {
         return weight;
     }
