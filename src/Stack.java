189ef9c52125c2402721c612d2d750e75d000c17
azizosharke <abdelaza@tcd.ie>
2022-04-05 14:57:08 +0100

    added class stack and connected it to search and main for stops

---
+++ src/Stack.java
@@ -0,0 +1,93 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/*  see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.*/
+
+public class Stack<Item> implements Iterable<Item> {
+    private Node<Item> first;     // top of stack
+    private int n;                // size of the stack
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+    public Stack() {
+        first = null;
+        n = 0;
+    }
+    /**
+     * Returns true if this stack is empty.
+     *
+     * @return true if this stack is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * Returns the number of items in this stack.
+     *
+     * @return the number of items in this stack
+     */
+    public int size() {
+        return n;
+    }
+    /**
+     * Adds the item to this stack.
+     *
+     * @param item the item to add
+     */
+    public void push(Item item) {
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    }
+    /**
+     * Returns a string representation of this stack.
+     *
+     * @return the sequence of items in this stack in LIFO order, separated by spaces
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    }
+
+    /**
+     * Returns an iterator to this stack that iterates through the items in LIFO order.
+     *
+     * @return an iterator to this stack that iterates through the items in LIFO order
+     */
+    public Iterator<Item> iterator() {
+        return new LinkedIterator(first);
+    }
+    private class LinkedIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public LinkedIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
+
+
